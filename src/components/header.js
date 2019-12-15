923c56006644c4e0e4fec2c0271ab07e0e376265
Fabricio Dupin <fabicio.dupin@gmail.com>
2019-12-15 03:07:30 -0300

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#72bfe5`,
       marginBottom: `1.45rem`,
     }}
   >
